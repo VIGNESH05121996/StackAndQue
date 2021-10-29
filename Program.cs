8d25673389ff776adb18c3e5b97e88fe0c08642e
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-10-29 19:11:43 +0530

    [VIGNESH M] ADDED: Add and Display Method

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace StackAndQue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome To Stack And Que\n");
+            LinkedListStack stack = new LinkedListStack();
+            stack.Add(70);
+            stack.Add(30);
+            stack.Add(56);
+            Console.WriteLine("\nPushed 56 on top of the stack");
+            stack.Display();
+            Console.ReadLine();
         }
     }
 }
